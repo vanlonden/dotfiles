365eb7b2145d64436b2218df2aa1a05a60b36793
Jordy van Londen <jordy.vanlonden@gmail.com>
2017-11-05 15:57:48 +0100

    ...

--- .config/zsh/zshrc.sh
+++ .config/zsh/zshrc.sh
@@ -4,7 +4,7 @@ SAVEHIST=1000
 setopt inc_append_history # To save every command before it is executed 
 setopt share_history # setopt inc_append_history
 
-source ~/dotfiles/zsh/plugins/fixls.zsh
+source ~/.config/zsh/plugins/fixls.zsh
 
 #Functions
 # Custom cd
@@ -17,13 +17,13 @@ alias cd="c"
 # For vim mappings: 
 stty -ixon
 
-source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/history.zsh
-source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
-source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
-source ~/dotfiles/zsh/plugins/vi-mode.plugin.zsh
-source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/dotfiles/zsh/keybindings.sh
+source ~/.config/zsh/plugins/oh-my-zsh/lib/history.zsh
+source ~/.config/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
+source ~/.config/zsh/plugins/oh-my-zsh/lib/completion.zsh
+source ~/.config/zsh/plugins/vi-mode.plugin.zsh
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.config/zsh/keybindings.sh
 
 # Fix for arrow-key searching
 # start typing + [Up-Arrow] - fuzzy find history forward
@@ -39,4 +39,4 @@ if [[ "${terminfo[kcud1]}" != "" ]]; then
 	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
-source ~/dotfiles/zsh/prompt.sh
+source $HOME/.config/zsh/prompt.sh
--- .zshrc
+++ .zshrc
@@ -1,5 +1,6 @@
 alias careweb-maccloud-tunnel="ssh -L 8081:localhost:8081 -L 8111:localhost:8111 git@maccloud"
-alias config="/usr/bin/git --git-dir=/home/vanlonden/.cfg/ --work-tree=/home/vanlonden"
+
+alias config="git --git-dir=$HOME/src/dotfiles --work-tree=$HOME"
 
 alias ls="ls --color=auto"
 alias ll="ls -l"
@@ -12,6 +13,6 @@ alias vim="nvim"
 
 alias y="yaourt"
 
-source ~/.config/zsh/zshrc.sh
+source $HOME/.config/zsh/zshrc.sh
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
